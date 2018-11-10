5170b92fef4b22fc0018922f7ac92612c7dcfd6c
SonyChen010 <36293204+SonyChen010@users.noreply.github.com>
2018-11-10 02:05:23 -0500

    Create Light_on_&_off.ino

---
+++ Analog Example/Light_on_&_off.ino
@@ -0,0 +1,36 @@
+/**************************************************************************
+ * Textile Game Controller workshop 
+ * Working with Arduino Micro board and conductive materials
+ * Analog switch example
+ * Arduino serial communication with Processing 
+ * Arduino Example code:
+ * https://www.arduino.cc/en/Tutorial/SerialCallResponse
+ * Social Body Lab, 2018 Nov.
+**************************************************************************/
+
+char val;
+int felt = 0;
+int value = 0;
+int inByte = 0;
+
+void setup() {
+Serial.begin(9600);
+establishContact();
+}
+
+void loop() {
+if(Serial.available()>0);{
+inByte=Serial.read();
+
+value= analogRead (felt);
+value = value/4;
+delay(10);
+}
+Serial.write(value);
+}
+
+void establishContact(){
+while(Serial.available()<=0){
+Serial.print('A');
+delay(300);
+}}
